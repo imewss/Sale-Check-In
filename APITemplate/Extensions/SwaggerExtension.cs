770ce6dac62b96d62c3f02ba26cfad9e3bd9749e
Mew <imew33@gmail.com>
2020-11-16 13:07:51 +0700

    - add Nlog and remove unuse file

---
+++ APITemplate/Extensions/SwaggerExtension.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APITemplate.Extensions
+{
+    public static class SwaggerExtension
+    {
+        /// <summary>
+        /// Adds the custom swagger.
+        /// </summary>
+        /// <param name="services">The services.</param>
+        /// <param name="configuration">Configuration file</param>
+        public static void AddCustomSwagger(this IServiceCollection services, IConfiguration configuration)
+        {
+           
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "Sample API",
+                    Description = "A simple example ASP.NET Core Web API",
+                    TermsOfService = new Uri("https://example.com/terms"),
+                
+                });
+            });
+        }
+
+        /// <summary>
+        /// User custom middleware to serve generated Swagger as a JSON endpoint. And Enable middleware to serve swagger-ui
+        /// </summary>
+        /// <param name="app">Application param</param>
+        /// <param name="configuration">Configuration file</param>
+        public static void UseCustomerSwagger(this IApplicationBuilder app, IConfiguration configuration)
+        {
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.), 
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("../swagger/v1/swagger.json", "Pay Society Sample Service/Gateway");
+            });
+        }
+    }
+}
--- APITemplate/Startup.cs
+++ APITemplate/Startup.cs
@@ -1,31 +1,35 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using System;
+using System.Reflection;
+using System.IO;
+using Microsoft.OpenApi.Models;
+using APITemplate.Extensions;
+using APITemplate.Interface;
+using APITemplate.Services;
 
 namespace APITemplate
 {
     public class Startup
     {
+        public IConfiguration _configuration { get; }
+
         public Startup(IConfiguration configuration)
         {
-            Configuration = configuration;
+            _configuration = configuration;
         }
-
-        public IConfiguration Configuration { get; }
-
+      
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddCustomSwagger(_configuration);
+
+            // regis logger
+            services.AddSingleton<ILoggerService, LoggerService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -38,8 +42,10 @@ namespace APITemplate
 
             app.UseHttpsRedirection();
 
-            app.UseRouting();
+            app.UseCustomerSwagger(_configuration);
 
+            app.UseRouting();
+          
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
