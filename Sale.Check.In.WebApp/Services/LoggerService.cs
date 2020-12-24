7e93a4d1d203a4eb04113abd3162f6850c084d64
Mew <imew33@gmail.com>
2020-12-24 18:57:50 +0700

    - add nlog

---
+++ Sale.Check.In.WebApp/Services/LoggerService.cs
@@ -0,0 +1,29 @@
+﻿using NLog;
+using Sale.Check.In.WebApp.Interface;
+
+namespace Sale.Check.In.WebApp.Services
+{
+    public class LoggerService : ILoggerService
+    {
+        private static ILogger logger = LogManager.GetCurrentClassLogger();
+        public void LogDebug(string message)
+        {
+            logger.Debug(message);
+        }
+
+        public void LogError(string message)
+        {
+            logger.Error(message);
+        }
+
+        public void LogInfo(string message)
+        {
+            logger.Info(message);
+        }
+
+        public void LogWarn(string message)
+        {
+            logger.Warn(message);
+        }
+    }
+}
--- Sale.Check.In.WebApp/Startup.cs
+++ Sale.Check.In.WebApp/Startup.cs
@@ -1,10 +1,17 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.SpaServices.AngularCli;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.IdentityModel.Tokens;
+using Sale.Check.In.WebApp.DBContexts;
+using Sale.Check.In.WebApp.Interface;
+using Sale.Check.In.WebApp.Managers;
+using Sale.Check.In.WebApp.Services;
+using System.Text;
 
 namespace Template
 {
@@ -21,11 +28,53 @@ namespace Template
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+
+            string mySqlConnectionStr = Configuration.GetConnectionString("DefaultConnection");
+            services.AddDbContextPool<MyDBContext>(options => options.UseMySql(mySqlConnectionStr, ServerVersion.AutoDetect(mySqlConnectionStr)));
+
+            services.AddAuthentication(opt =>
+            {
+                opt.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                opt.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+             .AddJwtBearer(options =>
+                {
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuer = true,
+                        ValidateAudience = true,
+                        ValidateLifetime = true,
+                        ValidateIssuerSigningKey = true,
+
+                        ValidIssuer = "https://localhost:44388",
+                        ValidAudience = "https://localhost:44388",
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("superSecretKey@345"))
+                    };
+                });
+
+            // regis logger
+            services.AddSingleton<ILoggerService, LoggerService>();
+
+            // regis manager
+            services.AddScoped<ICheckInManager, CheckInManager>();
+            services.AddScoped<IUserManager, UserManager>();
+
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy",
+                    builder => builder.AllowAnyOrigin()
+                    .AllowAnyMethod()
+                    .AllowAnyHeader());
+            });
+
+
             // In production, the Angular files will be served from this directory
             services.AddSpaStaticFiles(configuration =>
             {
                 configuration.RootPath = "ClientApp/dist";
             });
+
+            services.AddHttpContextAccessor();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -50,6 +99,15 @@ namespace Template
             }
 
             app.UseRouting();
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.UseCors(builder => builder
+                .AllowAnyHeader()
+                .AllowAnyMethod()
+                .SetIsOriginAllowed((host) => true)
+                .AllowCredentials()
+            );
 
             app.UseEndpoints(endpoints =>
             {
